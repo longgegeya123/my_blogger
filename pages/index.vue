8eb7028ed5c677bbef3b492cfa73cfb8265e01b3
longgegeya123 <971239524@qq.com>
2022-10-04 14:00:44 +0800

    项目搭建

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>你好{{url}}</h1>
+    <h2>{{$store.state.filterName}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      url: ''
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+      this.url = process.env.baseUrl;
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  }
+};
+</script>
---
+++ store/index.js
@@ -0,0 +1,14 @@
+const state = () => {
+  return {
+    filterName: '咩咩咩咩'
+  }
+}
+const mutations = {}
+const actions = {}
+const getters = {}
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
